5a65531517a35c4db445122e54789b53a8a811cb
Gustavo Paes <gustavo.silva@di2win.com>
2025-02-18 08:52:57 -0300

    convertendo a funçaõ em uma api com url

--- index.mjs
+++ index.mjs
@@ -1,11 +1,14 @@
 import { log } from './log.mjs';
 
 export const handler = async (event) => {
-  log( JSON.stringify(event))
+  log( JSON.stringify(event));
 
   return {
     statusCode: 200,
-    body: JSON.stringify(event),
+    body: <html><body> Dados de requisicao ${ JSON.stringify(event) }</body></html>,
+    headers:{
+      "content-type":"text/html"
+    }
 
   }
 };
